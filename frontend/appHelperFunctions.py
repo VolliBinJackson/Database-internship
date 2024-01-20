38317578bbb3c0f6c592c80c4a9eb9c1f8891d4a
Volkan <volkanitmec0@gmail.com>
2024-01-20 06:51:31 +0100

    Added query_item() function to query items

--- frontend/appHelperFunctions.py
+++ frontend/appHelperFunctions.py
@@ -1,5 +1,7 @@
 import sqlite3 as sql
 
+
+# Check if User is already registered
 def isUserRegistered(username, password):
     con = sql.connect("database.db")
     cur = con.cursor()
@@ -12,6 +14,8 @@ def isUserRegistered(username, password):
     else:
         return False
     
+
+# Same scenario, but for restaurants    
 def isRestaurantRegistered(name, password):
     con = sql.connect("database.db")
     cur = con.cursor()
@@ -26,7 +30,7 @@ def isRestaurantRegistered(name, password):
         return None
     
 
-
+# Retrieve items from specific restaurant
 def get_items_for_restaurant(restaurant_id):
     with sql.connect('database.db') as con:
         cur = con.cursor()
@@ -35,9 +39,20 @@ def get_items_for_restaurant(restaurant_id):
     return items
 
 
+# Check if this specific restaurant has this item
 def is_item_in_restaurant(item_name, restaurant_id):
     with sql.connect('database.db') as con:
         cur = con.cursor()
         cur.execute("SELECT COUNT(*) FROM items WHERE ItemName=? AND RestaurantID=?", (item_name, restaurant_id))
         count = cur.fetchone()[0]
     return count > 0
+
+
+# simple query
+def query_items():
+    conn = sql.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM items')
+    items = cursor.fetchall()
+    conn.close()
+    return items
